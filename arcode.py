3a599f45b91793e1723140a16816ac2d987783f8
Alexandria Redmon <alexandriadredmon@gmail.com>
2024-06-09 23:17:18 -0400

    Formatting

--- arcode.py
+++ arcode.py
@@ -14,29 +14,59 @@ from litellm.llms.openai import OpenAIError
 from config import get_api_keys
 from lib.argument_parser import parse_arguments
 from lib.gitignore_parser import parse_gitignore
-from lib.file_util import is_in_middle_of_file, print_tree, get_files, format_file_contents, is_binary_file, is_ignored, parse_files, extract_estimated_characters, calculate_line_difference
-from lib.prompt_templates import AUTODEV_PROMPT_PRE, AUTODEV_PROMPT_POST_TEMPLATE, QUESTION_PROMPT_PRE, QUESTION_PROMPT_POST_TEMPLATE
+from lib.file_util import (
+    extract_filename_start,
+    extract_filename_end,
+    is_in_middle_of_file,
+    print_tree,
+    get_files,
+    format_file_contents,
+    is_binary_file,
+    is_ignored,
+    parse_files,
+    extract_estimated_characters,
+    calculate_line_difference,
+)
+from lib.prompt_templates import (
+    AUTODEV_PROMPT_PRE,
+    AUTODEV_PROMPT_POST_TEMPLATE,
+    QUESTION_PROMPT_PRE,
+    QUESTION_PROMPT_POST_TEMPLATE,
+)
 from lib.litellm_client import create_litellm_client, calculate_token_count
 from lib.shell_util import (
-    LIGHT_PINK, LIGHT_GREEN, LIGHT_RED, LIGHT_BLUE, RESET_COLOR, WHITE_ON_DARK_BLUE, BLACK_ON_WHITE,
-    WHITE_ON_BLACK, LIGHT_ORANGE, BLACK_BACKGROUND, BLACK_ON_LIGHT_ORANGE, BLACK_ON_LIGHT_GREEN
+    LIGHT_PINK,
+    LIGHT_GREEN,
+    LIGHT_RED,
+    LIGHT_BLUE,
+    RESET_COLOR,
+    WHITE_ON_DARK_BLUE,
+    BLACK_ON_WHITE,
+    WHITE_ON_BLACK,
+    LIGHT_ORANGE,
+    BLACK_BACKGROUND,
+    BLACK_ON_LIGHT_ORANGE,
+    BLACK_ON_LIGHT_GREEN,
 )
 from lib.file_writer import write_files
 
 from lib.embedding_util import get_top_relevant_files
+
 
 def main():
     args = parse_arguments()
 
     if not args.requirements:
         if sys.stdin.isatty():
-            requirements = ' '.join(args.requirements)
+            requirements = " ".join(args.requirements)
         else:
             requirements = sys.stdin.read().strip()
     else:
-        requirements = ' '.join(args.requirements).strip()
+        requirements = " ".join(args.requirements).strip()
 
-    ignore_patterns = parse_gitignore(os.path.join(args.dir, '.gitignore'), args.ignore)
+    ignore_patterns = parse_gitignore(
+        os.path.join(args.dir, ".gitignore"), args.ignore
+    )
 
     # Validate and fetch the API keys for the provided model
     api_keys = get_api_keys(args.model)
@@ -44,20 +74,23 @@ def main():
     file_contents = None
     startpath = args.dir
 
-    print(f"\n{WHITE_ON_BLACK} 🏗️  {BLACK_ON_WHITE} BUILDING FEATURE(S): {RESET_COLOR}")
-    print(f"    {LIGHT_PINK}> {LIGHT_BLUE}{requirements}{RESET_COLOR}")
-
-    print(f"\n{WHITE_ON_BLACK} ⚙️  {BLACK_ON_WHITE} CONFIGURATION: {RESET_COLOR}")
-    print(f"{LIGHT_PINK}        Config file: {LIGHT_BLUE}{args.config_from_file}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}          Directory: {LIGHT_BLUE}{args.dir}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}              Model: {LIGHT_BLUE}{args.model}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}    Embedding Model: {LIGHT_BLUE}{args.model_embedding}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}     Token Encoding: {LIGHT_BLUE}{args.token_encoding}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}         Auto-write: {LIGHT_BLUE}{args.autowrite}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}            Focused: {LIGHT_BLUE}{args.focused}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}             Ignore: {LIGHT_BLUE}{args.ignore}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}               Mode: {LIGHT_BLUE}{args.mode}{RESET_COLOR}")
-    print(f"{LIGHT_PINK}          Resources: {LIGHT_BLUE}{args.resources}{RESET_COLOR}")
+    print(
+        f"""
+{WHITE_ON_BLACK} 🏗️  {BLACK_ON_WHITE} BUILDING FEATURE(S): {RESET_COLOR}
+{LIGHT_PINK}> {LIGHT_BLUE}{requirements}{RESET_COLOR}
+{WHITE_ON_BLACK} ⚙️  {BLACK_ON_WHITE} CONFIGURATION: {RESET_COLOR}
+{LIGHT_PINK}        Config file: {LIGHT_BLUE}{args.config_from_file}{RESET_COLOR}
+{LIGHT_PINK}          Directory: {LIGHT_BLUE}{args.dir}{RESET_COLOR}
+{LIGHT_PINK}              Model: {LIGHT_BLUE}{args.model}{RESET_COLOR}
+{LIGHT_PINK}    Embedding Model: {LIGHT_BLUE}{args.model_embedding}{RESET_COLOR}
+{LIGHT_PINK}     Token Encoding: {LIGHT_BLUE}{args.token_encoding}{RESET_COLOR}
+{LIGHT_PINK}         Auto-write: {LIGHT_BLUE}{args.autowrite}{RESET_COLOR}
+{LIGHT_PINK}            Focused: {LIGHT_BLUE}{args.focused}{RESET_COLOR}
+{LIGHT_PINK}             Ignore: {LIGHT_BLUE}{args.ignore}{RESET_COLOR}
+{LIGHT_PINK}               Mode: {LIGHT_BLUE}{args.mode}{RESET_COLOR}
+{LIGHT_PINK}          Resources: {LIGHT_BLUE}{args.resources}{RESET_COLOR}
+    """
+    )
 
     if args.focused:
         files = get_top_relevant_files(
@@ -65,19 +98,25 @@ def main():
             ignore_patterns=ignore_patterns,
             query=requirements,
             num_files=args.focused,
-            model_embedding=args.model_embedding
+            model_embedding=args.model_embedding,
         )
-        print(f"\n{WHITE_ON_BLACK} 🔬 {BLACK_ON_WHITE} FOCUSING ON {args.focused} MOST RELEVANT FILE CHUNKS: {RESET_COLOR}")
+        print(
+            f"\n{WHITE_ON_BLACK} 🔬 {BLACK_ON_WHITE} FOCUSING ON {args.focused} MOST RELEVANT FILE CHUNKS: {RESET_COLOR}"
+        )
         for file in files:
             path = file["path"]
             score = round(file["score"], 2)
-            print(f"    {LIGHT_PINK}* {LIGHT_BLUE}{path} {LIGHT_GREEN}({score}){RESET_COLOR}")
+            print(
+                f"    {LIGHT_PINK}* {LIGHT_BLUE}{path} {LIGHT_GREEN}({score}){RESET_COLOR}"
+            )
     else:
         files = get_files(startpath, ignore_patterns)
 
     f = io.StringIO()
     if not args.focused or args.focused < 1:
-        print(f"\n{WHITE_ON_BLACK} 📂 {BLACK_ON_WHITE} FILES FOUND: {RESET_COLOR}")
+        print(
+            f"\n{WHITE_ON_BLACK} 📂 {BLACK_ON_WHITE} FILES FOUND: {RESET_COLOR}"
+        )
         for file in files:
             path = file["path"]
             print(f"    {LIGHT_PINK}* {LIGHT_BLUE}{path}{RESET_COLOR}")
@@ -88,7 +127,7 @@ def main():
                 try:
                     response = requests.get(url)
                     response.raise_for_status()
-                    soup = BeautifulSoup(response.content, 'html.parser')
+                    soup = BeautifulSoup(response.content, "html.parser")
                     body_content = str(soup.body)
                     print(f"\nURL: {url}\n{body_content}")
                 except requests.RequestException as e:
@@ -106,51 +145,89 @@ def main():
         print(format_file_contents(files))
 
         if args.mode == "question":
-            prompt_post = QUESTION_PROMPT_POST_TEMPLATE.format(requirements=requirements)
+            prompt_post = QUESTION_PROMPT_POST_TEMPLATE.format(
+                requirements=requirements
+            )
         else:
-            prompt_post = AUTODEV_PROMPT_POST_TEMPLATE.format(requirements=requirements)
+            prompt_post = AUTODEV_PROMPT_POST_TEMPLATE.format(
+                requirements=requirements
+            )
 
         print(prompt_post)
 
         USER_CONTENT = f.getvalue()
 
-    answers = {
-        "next_step": None
-    }
-    messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": USER_CONTENT}]
+    answers = {"next_step": None}
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": USER_CONTENT},
+    ]
 
     client = create_litellm_client(args.model)
 
-    input_tokens, output_tokens, total_tokens = calculate_token_count(args.model, messages, args.token_encoding)
-    print(f"\n{WHITE_ON_BLACK} 🧮 {BLACK_ON_WHITE} TOKENS ({total_tokens:,} total) [{args.token_encoding}]{RESET_COLOR}")
-    print(f"     {LIGHT_PINK}In: {LIGHT_BLUE}{input_tokens:,}{RESET_COLOR}")
-    print(f"    {LIGHT_PINK}Out: {LIGHT_BLUE}{output_tokens:,}{RESET_COLOR}\n")
+    input_tokens, output_tokens, total_tokens = calculate_token_count(
+        args.model, messages, args.token_encoding
+    )
+    print(
+        f"""
+{WHITE_ON_BLACK} 🧮 {BLACK_ON_WHITE} TOKENS ({total_tokens:,} total) [{args.token_encoding}]{RESET_COLOR}
+    {LIGHT_PINK}In: {LIGHT_BLUE}{input_tokens:,}{RESET_COLOR}
+    {LIGHT_PINK}Out: {LIGHT_BLUE}{output_tokens:,}{RESET_COLOR}
+
+                """
+    )
     while answers["next_step"] != "🚪 Exit":
         try:
-            completion = client(model=args.model, messages=messages, stream=True)
+            completion = client(
+                model=args.model, messages=messages, stream=True
+            )
 
-            print(f"\n{WHITE_ON_BLACK} 🌐 {BLACK_ON_WHITE} STREAMING RESPONSE: {RESET_COLOR}")
+            print(
+                f"\n{WHITE_ON_BLACK} 🌐 {BLACK_ON_WHITE} STREAMING RESPONSE: {RESET_COLOR}"
+            )
             streamed_response = ""
             response_chunks = []
-            print("    ", end="", flush=True)
+            output_padding = "    "
+            print(f"{LIGHT_BLUE}", end="", flush=True)
+            printed_lines = []
+            since_last_line = ""
+            latest_line = None
             for chunk in completion:
-                if chunk and chunk.choices and chunk.choices[0] and chunk.choices[0].delta:
+                if (
+                    chunk
+                    and chunk.choices
+                    and chunk.choices[0]
+                    and chunk.choices[0].delta
+                ):
                     delta = chunk.choices[0].delta
-                    if delta.get('content'):
-                        content = delta['content']
-                        streamed_response += content
-                        if is_in_middle_of_file(streamed_response):
-                            print(LIGHT_ORANGE, end="", flush=True)
-                        else:
-                            print(
-                                f"{RESET_COLOR}{BLACK_BACKGROUND}{LIGHT_BLUE}",
-                                end="",
-                                flush=True
-                            )
+                    if delta.get("content"):
+                        content = delta["content"]
                         response_chunks.append(content)
-                        printed_content = content.replace("\n", "\n    ")
-                        print(f"{printed_content}{RESET_COLOR}", end="", flush=True)
-                        
+                        streamed_response += content
+                        since_last_line += content
+                        while "\n" in since_last_line:
+                            bits = since_last_line.splitlines()
+                            latest_line = bits[0]
+                            is_file_header = (
+                                "===.= ==== FILENAME: " in latest_line
+                                and extract_filename_start(latest_line)
+                            )
+                            is_file_footer = (
+                                "===.= ==== EOF: " in latest_line
+                                and extract_filename_end(latest_line)
+                            )
+                            if is_file_header or is_file_footer:
+                                print(LIGHT_PINK, end="", flush=True)
+
+                            print(f"{output_padding}{latest_line}")
+
+                            if is_file_header:
+                                print(LIGHT_ORANGE, end="", flush=True)
+                            if is_file_footer:
+                                print(LIGHT_BLUE, end="", flush=True)
+
+                            since_last_line = "".join(bits[1:])
+            print(since_last_line)
 
         except OpenAIError as e:
             print(f"{BLACK_BACKGROUND}{LIGHT_RED}{e.message}{RESET_COLOR}")
@@ -160,21 +237,28 @@ def main():
         messages.append({"role": "assistant", "content": streamed_response})
 
         if sys.stdin.isatty():
-            choices = ['💬 Followup prompt', '🏗️  Write changeset to files', '📑 Copy full response']
-
+            choices = [
+                "💬 Followup prompt",
+                "🏗️  Write changeset to files",
+                "📑 Copy full response",
+            ]
 
             for file in files:
                 filename = file["filename"]
                 choices.append(f"📄 Copy file {filename}")
 
             if len(files):
-                print(f"\n\n{WHITE_ON_BLACK} 📁 {BLACK_ON_WHITE} FILES TO UPDATE: {RESET_COLOR}")
+                print(
+                    f"\n\n{WHITE_ON_BLACK} 📁 {BLACK_ON_WHITE} FILES TO UPDATE: {RESET_COLOR}"
+                )
 
             # Print file changes
             for file in files:
                 filename = file["filename"]
                 new_content = file["contents"]
-                line_diff = calculate_line_difference(os.path.join(args.dir, filename), new_content)
+                line_diff = calculate_line_difference(
+                    os.path.join(args.dir, filename), new_content
+                )
                 line_diff_str = f"    {LIGHT_PINK}* {LIGHT_GREEN}{filename} ({line_diff:+d}){RESET_COLOR}"
                 print(line_diff_str)
                 print("")
@@ -185,47 +269,63 @@ def main():
 
             questions = [
                 {
-                    'type': 'list',
-                    'name': 'next_step',
-                    'message': '↕',
-                    'choices': choices,
+                    "type": "list",
+                    "name": "next_step",
+                    "message": "↕",
+                    "choices": choices,
                 }
             ]
-            
-            input_tokens, output_tokens, total_tokens = calculate_token_count(args.model, messages, args.token_encoding)
-            print(f"\n{WHITE_ON_BLACK} 🧮 {BLACK_ON_WHITE} TOKENS ({total_tokens:,} total) [{args.token_encoding}]{RESET_COLOR}")
-            print(f"     {LIGHT_PINK}In: {LIGHT_BLUE}{input_tokens:,}{RESET_COLOR}")
-            print(f"    {LIGHT_PINK}Out: {LIGHT_BLUE}{output_tokens:,}{RESET_COLOR}\n")
 
-            print(f"{WHITE_ON_BLACK} ⚡️ {BLACK_ON_WHITE} ACTION: {RESET_COLOR}")
+            input_tokens, output_tokens, total_tokens = calculate_token_count(
+                args.model, messages, args.token_encoding
+            )
+            print(
+                f"""
+{WHITE_ON_BLACK} 🧮 {BLACK_ON_WHITE} TOKENS ({total_tokens:,} total) [{args.token_encoding}]{RESET_COLOR}
+    {LIGHT_PINK}In: {LIGHT_BLUE}{input_tokens:,}{RESET_COLOR}
+    {LIGHT_PINK}Out: {LIGHT_BLUE}{output_tokens:,}{RESET_COLOR}
+
+                """
+            )
+
+            print(
+                f"{WHITE_ON_BLACK} ⚡️ {BLACK_ON_WHITE} ACTION: {RESET_COLOR}"
+            )
             exit_menu = False
             if args.autowrite:
                 write_files(files, args.dir)
-                print(f"\n{WHITE_ON_BLACK} ✅ {BLACK_ON_WHITE} CHANGESET WRITTEN {RESET_COLOR}")
+                print(
+                    f"\n{WHITE_ON_BLACK} ✅ {BLACK_ON_WHITE} CHANGESET WRITTEN {RESET_COLOR}"
+                )
 
             while not exit_menu:
                 answers = prompt(questions)
-                if answers['next_step'] == '📑 Copy full response':
+                if answers["next_step"] == "📑 Copy full response":
                     pyperclip.copy(streamed_response)
                     print("Response copied to clipboard.")
-                elif answers['next_step'].startswith("📄 Copy file "):
+                elif answers["next_step"].startswith("📄 Copy file "):
                     for file in files:
                         filename = file["filename"]
-                        if answers['next_step'] == f"📄 Copy file {filename}":
+                        if answers["next_step"] == f"📄 Copy file {filename}":
                             pyperclip.copy(file["contents"])
-                elif answers['next_step'] == '🏗️  Write changeset to files':
+                elif answers["next_step"] == "🏗️  Write changeset to files":
                     write_files(files, args.dir)
-                    print(f"\n{WHITE_ON_BLACK} ✅ {BLACK_ON_WHITE} CHANGESET WRITTEN {RESET_COLOR}")
-                elif answers['next_step'] == "💬 Followup prompt":
+                    print(
+                        f"\n{WHITE_ON_BLACK} ✅ {BLACK_ON_WHITE} CHANGESET WRITTEN {RESET_COLOR}"
+                    )
+                elif answers["next_step"] == "💬 Followup prompt":
                     followup = input(">")
                     messages.append({"role": "user", "content": followup})
                     exit_menu = True
-                elif answers['next_step'] == "🚪 Exit":
+                elif answers["next_step"] == "🚪 Exit":
                     exit_menu = True
     else:
         if args.autowrite:
             write_files(files, args.dir)
-            print(f"\n{WHITE_ON_BLACK} ✅ {BLACK_ON_WHITE} CHANGESET WRITTEN {RESET_COLOR}")
+            print(
+                f"\n{WHITE_ON_BLACK} ✅ {BLACK_ON_WHITE} CHANGESET WRITTEN {RESET_COLOR}"
+            )
+
 
 if __name__ == "__main__":
     main()
--- lib/file_util.py
+++ lib/file_util.py
@@ -2,55 +2,77 @@ import os
 import re
 from .gitignore_parser import is_ignored
 
-binary_extensions = {
-    '.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.ico',
-    '.mp3', '.wav', '.flac', '.aac', '.ogg', '.wma',
-    '.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.mpg', '.mpeg',
-    '.pdf', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.odt', '.ods', '.odp',
-    '.zip', '.rar', '.tar', '.gz', '.7z', '.bz2', 
-    '.exe', '.dll', '.so', '.bin', '.dmg', '.iso', '.img', 
-    '.psd', '.ai', '.indd', '.sketch',
-    '.swf', '.fla',
-    '.ttf', '.otf', '.woff', '.woff2', 
-    '.class', '.jar', 
-    '.dat', '.bak', 
-    '.cr2', '.nef', '.arw', '.dng', 
-    '.cab', '.cpl', '.cur', '.deskthemepack', '.dll', '.dmp', '.drv', '.efi', '.exe', 
-    '.resx', '.resource', 
-    '.db', '.sqlite', 
-    '.pkg', '.deb', '.rpm', 
-    '.apk', 
-    '.ipa', 
-    '.crx', 
-    '.vsix', 
-    '.xpi', 
-    '.msi', 
-    '.part', 
-}
+from .constants import binary_extensions
 
 file_parse_pattern = re.compile(
     r"===\.= ==== FILENAME: (?P<filename>.*?) = ===== =========\n```.*?\n(?P<content>.*?)\n```\n===\.= ==== EOF: (?P=filename) = ===== =========",
-    re.DOTALL
+    re.DOTALL,
 )
 # Define the pattern for the starting token
-middle_of_file_start_pattern = re.compile(r"===\.= ==== FILENAME: .*? = ===== =========\n```(.*?)\n", re.DOTALL)
+middle_of_file_start_pattern = re.compile(
+    r"===\.= ==== FILENAME: .*? = ===== =========\n```(.*?)\n", re.DOTALL
+)
 # Define the pattern for the closing token
-middle_of_file_end_pattern = re.compile(r"===\.= ==== EOF: .*? = ===== =========\n", re.DOTALL)
+middle_of_file_end_pattern = re.compile(
+    r"===\.= ==== EOF: .*? = ===== =========\n", re.DOTALL
+)
+
+file_start_token = re.compile(
+    r"===\.= ==== FILENAME: .*? = ===== =========", re.DOTALL
+)
+
+file_end_token = re.compile(
+    r"===\.= ==== EOF: .*? = ===== =========", re.DOTALL
+)
+
+
+def extract_filename_start(text):
+    matches = file_start_token.findall(text)
+    if len(matches) == 1:
+        return matches[0]
+    else:
+        return None
+
+
+def extract_filename_end(text):
+    matches = file_end_token.findall(text)
+    if len(matches) == 1:
+        return matches[0]
+    else:
+        return None
+
 
 def is_binary_file(filename):
     return os.path.splitext(filename)[1].lower() in binary_extensions
 
-def print_tree(startpath, ignore_patterns, prefix=''):
-    for root, dirs, files in os.walk(startpath):
-        dirs[:] = [d for d in dirs if not is_ignored(os.path.relpath(os.path.join(root, d), startpath), ignore_patterns)]
-        files = [f for f in files if not is_ignored(os.path.relpath(os.path.join(root, f), startpath), ignore_patterns) and not is_binary_file(f)]
 
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * (level)
-        print(f'{prefix}{indent}{os.path.basename(root)}/')
-        subindent = ' ' * 4 * (level + 1)
+def print_tree(startpath, ignore_patterns, prefix=""):
+    for root, dirs, files in os.walk(startpath):
+        dirs[:] = [
+            d
+            for d in dirs
+            if not is_ignored(
+                os.path.relpath(os.path.join(root, d), startpath),
+                ignore_patterns,
+            )
+        ]
+        files = [
+            f
+            for f in files
+            if not is_ignored(
+                os.path.relpath(os.path.join(root, f), startpath),
+                ignore_patterns,
+            )
+            and not is_binary_file(f)
+        ]
+
+        level = root.replace(startpath, "").count(os.sep)
+        indent = " " * 4 * (level)
+        print(f"{prefix}{indent}{os.path.basename(root)}/")
+        subindent = " " * 4 * (level + 1)
         for f in files:
-            print(f'{prefix}{subindent}{f}')
+            print(f"{prefix}{subindent}{f}")
+
 
 def format_file_contents(files):
     contents = ""
@@ -61,28 +83,49 @@ def format_file_contents(files):
         contents += f"\n**************** EOF: {file_path} ****************\n"
     return contents
 
+
 def get_files(startpath, ignore_patterns):
     all_files = []
     for root, _, files in os.walk(startpath):
-        files = [f for f in files if not is_ignored(os.path.relpath(os.path.join(root, f), startpath), ignore_patterns) and not is_binary_file(f)]
+        files = [
+            f
+            for f in files
+            if not is_ignored(
+                os.path.relpath(os.path.join(root, f), startpath),
+                ignore_patterns,
+            )
+            and not is_binary_file(f)
+        ]
         for f in files:
             file_path = os.path.relpath(os.path.join(root, f), startpath)
             try:
-                with open(os.path.join(root, f), 'r', encoding='utf-8', errors='ignore') as file:
-                    all_files.append({
-                        "path": file_path,
-                        "data": file.read(),
-                    })
+                with open(
+                    os.path.join(root, f),
+                    "r",
+                    encoding="utf-8",
+                    errors="ignore",
+                ) as file:
+                    all_files.append(
+                        {
+                            "path": file_path,
+                            "data": file.read(),
+                        }
+                    )
             except UnicodeDecodeError as e:
                 print(f"Error reading file {file_path}: {e}")
             except Exception as e:
                 print(f"Unknown error reading file: {file_path}: {e}")
     return all_files
 
+
 def parse_files(string):
     matches = file_parse_pattern.findall(string)
-    files = [{"filename": match[0].strip(), "contents": match[1].strip()} for match in matches]
+    files = [
+        {"filename": match[0].strip(), "contents": match[1].strip()}
+        for match in matches
+    ]
     return files
+
 
 def is_in_middle_of_file(string):
     # Find all starting and closing tokens
@@ -94,21 +137,23 @@ def is_in_middle_of_file(string):
         return True
     return False
 
+
 def extract_estimated_characters(string):
-    pattern = re.compile(
-        r"## ESTIMATED CHARACTERS:\n(\d+)"
-    )
+    pattern = re.compile(r"## ESTIMATED CHARACTERS:\n(\d+)")
     match = pattern.search(string)
     if match:
         return int(match.group(1))
     return 0
 
+
 def calculate_line_difference(filepath, new_content):
     try:
-        with open(filepath, 'r', encoding='utf-8') as file:
+        with open(filepath, "r", encoding="utf-8") as file:
             current_content = file.read()
-        current_lines = current_content.count('\n')
-        new_lines = new_content.count('\n')
+        current_lines = current_content.count("\n")
+        new_lines = new_content.count("\n")
         return new_lines - current_lines
     except FileNotFoundError:
-        return len(new_content.splitlines())  # If file does not exist, return total lines as added
+        return len(
+            new_content.splitlines()
+        )  # If file does not exist, return total lines as added
